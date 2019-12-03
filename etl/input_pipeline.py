dc165491727441e8835e86529d6f5bf309eca385
Richard <richarms@ska.ac.za>
2019-12-03 14:29:09 +0200

    Update and rename rot_im.py to input_pipeline.py

---
+++ etl/input_pipeline.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tensorflow as tf
+
+import pathlib
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
