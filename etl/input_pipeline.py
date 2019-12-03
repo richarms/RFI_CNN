7f3e53983f88d8525a6b0f1dde3af3920b9b87ce
Richard <richarms@ska.ac.za>
2019-12-03 14:35:44 +0200

    Update input_pipeline.py

--- etl/input_pipeline.py
+++ etl/input_pipeline.py
@@ -1,8 +1,11 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import tensorflow as tf
-
-import pathlib
-import matplotlib.pyplot as plt
-import pandas as pd
 import numpy as np
+
+# load config from etl_config.yml
+import etl_config as cfg
+cfg.raw['']
+
+# create a tensorflow dataset from the numpy array
+dataset = tf.data.Dataset.from_tensor_slices()
