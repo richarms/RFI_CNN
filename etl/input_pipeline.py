49c5c3ffdc2c656a4b38b3f72c7a7065f1da5e02
Richard Armstrong <richarms@ska.ac.za>
2019-12-03 15:20:48 +0200

    create input pipeline

--- etl/input_pipeline.py
+++ etl/input_pipeline.py
@@ -2,10 +2,15 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 import tensorflow as tf
 import numpy as np
+import h5py as h5
 
 # load config from etl_config.yml
-import etl_config as cfg
-cfg.raw['']
+import etl_config as ecfg
+
+# open the h5 file
+with h5.File(ecfg.raw['input_dir'], 'r') as fp:
+    v_ast = fp['output/vis_clean'].value
+    v_rfi = fp['output/vis_dirty'].value
 
 # create a tensorflow dataset from the numpy array
-dataset = tf.data.Dataset.from_tensor_slices()
+dataset = tf.data.Dataset.from_tensor_slices(v_obs)
